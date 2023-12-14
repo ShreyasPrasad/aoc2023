c68238c5fa74b0a3de2cd58b6541302e149b74e0
Shreyas Prasad <shreyaskrishnaprasad@gmail.com>
2023-12-14 11:28:48 -0500

    Finish day 12 in rust

--- rustsolutions/src/day12twostar/mod.rs
+++ rustsolutions/src/day12twostar/mod.rs
@@ -1,4 +1,6 @@
 use std::fs::read_to_string;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn dp_base_case(dp: &mut  Vec<Vec<u64>>, record: &String, numbers: &Vec<usize>) {
     let n = record.len();
@@ -7,17 +9,40 @@ fn dp_base_case(dp: &mut  Vec<Vec<u64>>, record: &String, numbers: &Vec<usize>) 
 
     for start in (0..n).rev() {
         let end = start + group_size;
-        if n-start >= group_size {
-            if record[start..=start].contains(".") {
-                dp[m-1][start] = dp[m-1][start+1];
-            } else if !record[start..end].contains(".") && !record[end..n].contains("#"){
-                if !record[start..end].contains("?"){
-                    dp[m-1][start] = 1;
+        
+        if record[start..=start].contains(".") {
+            dp[m-1][start] = dp[m-1][start+1];
+        } else if record[start..=start].contains("?") {
+            if n-start >= group_size && (start == 0 || !record[start-1..=start-1].contains("#")) {
+                if !record[start..end].contains(".") {
+                    if !record[start..end].contains("#"){
+                        if dp[m-1][start+1] > 0 {
+                            if !record[end..n].contains("#") {
+                                dp[m-1][start] = 1 + dp[m-1][start+1];
+                            } else {
+                                dp[m-1][start] = dp[m-1][start+1];
+                            }
+                        } else if end == n || !record[end..n].contains("#") {
+                            dp[m-1][start] = 1
+                        }
+                    } else {
+                        if !record[end..n].contains("#") {
+                            dp[m-1][start] = 1 + dp[m-1][start+1];
+                        } else {
+                            dp[m-1][start] = dp[m-1][start+1];
+                        }
+                    }
                 } else {
-                    dp[m-1][start] = 1 + dp[m-1][start+1];
+                    dp[m-1][start] = dp[m-1][start+1];
                 }
-            } else if start < (n-1) && dp[m-1][start+1] > 0 && record[start..=start].contains("?"){
+            } else {
                 dp[m-1][start] = dp[m-1][start+1];
+            }
+        } else {
+            if n-start >= group_size && (start == 0 || !record[start-1..=start-1].contains("#")){
+                if !record[start..end].contains(".") && !record[end..n].contains("#"){
+                    dp[m-1][start] = 1;
+                }
             }
         }
     }
@@ -56,7 +81,7 @@ fn process_record(record: String, numbers: Vec<usize>) -> u64 {
                     if !record[j..end].contains(".") {
                         if (j == 0 || chars[j-1] != '#') && (end == n || chars[end] != '#'){
                             if end < n {
-                                dp[i][j] = dp[i+1][end+1] + dp[i][j+1];
+                                dp[i][j] = dp[i+1][end+1];
                             }
                         }
                     }
@@ -64,13 +89,15 @@ fn process_record(record: String, numbers: Vec<usize>) -> u64 {
             }
         }
     }
-    println!("{:?}", dp[0]);
+    // println!("{:?}", dp[0]);
     dp[0][0]
 }
 
 pub fn twostar_solution() {
     // Populate the grid and find the start square.
     let mut total = 0;
+    let mut file = File::create("output").expect("Error creating file.");
+
     for line in read_to_string("src/day12twostar/input").unwrap().lines() {
         let record_split: Vec<&str> = line.split_whitespace().collect();
         let record: String = record_split.first().unwrap().to_string();
@@ -81,14 +108,14 @@ pub fn twostar_solution() {
         let mut expanded_str = String::from(&record);
         let mut expanded_numbers : Vec<usize> = Vec::from(numbers.clone());
         
-        /*
         for _ in 0..4{
             expanded_str += &("?".to_owned() + &record);
             expanded_numbers.extend(numbers.clone());
         }
-        */
 
-        total += process_record(expanded_str, expanded_numbers);
+        let value = process_record(expanded_str, expanded_numbers);
+        writeln!(file, "{}", value).expect("Error writing to file.");
+        total += value;
     }
 
     print!("{}", total);
